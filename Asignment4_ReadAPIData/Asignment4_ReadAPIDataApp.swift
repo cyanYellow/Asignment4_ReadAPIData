34b4ba89072b55101a6e277c8cc8cbf728c997fa
cyanYellow <williecyanyellow@gmail.com>
2023-09-20 08:49:20 -0600

    Initial Commit

---
+++ Asignment4_ReadAPIData/Asignment4_ReadAPIDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asignment4_ReadAPIDataApp.swift
+//  Asignment4_ReadAPIData
+//
+//  Created by Willie Green on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Asignment4_ReadAPIDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Asignment4_ReadAPIData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Asignment4_ReadAPIData
+//
+//  Created by Willie Green on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
